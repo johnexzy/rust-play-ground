f5485078f9fbd7e749e846e237aadf4e87c89ea5
Johnexzy <obajohn75@gmail.com>
2022-07-15 16:30:47 +0100

    :alembic: (Added commtizen conventions) conventional standard using cz

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::io;
+// use std::io;
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
     let mut guess = String::new();
     let mut apple = 0;
-
-    io::stdin()
+    println!("{guess}");
+    std::io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You guessed: {guess}");
